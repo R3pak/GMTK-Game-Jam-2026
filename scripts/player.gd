36259418e9df75fbc816c2b9a03bbad8f7f699f5
R3pak <repaksawan@gmail.com>
2026-07-23 20:45:49 +0300

    Fixed Jump
    Fixed the jump so that you need to be grounded to actually jump.

--- scripts/player.gd
+++ scripts/player.gd
@@ -36,7 +36,7 @@ func get_input():
 	else:
 		speed = move_toward(speed, 250.0, decel)
 	
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jump()
 	
 	if Input.is_action_just_released("jump") and velocity.y < 0.0:
