1fe7f71e7e0361241cfd1e5da9e5c17d2c6f5861
R3pak <repaksawan@gmail.com>
2026-07-26 23:57:02 +0300

    Final Polish
    Polished levels and added lose state and music.

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+func _ready() -> void:
+	$AudioStreamPlayer2D.play()
+	var player = get_tree().get_first_node_in_group("Player")  # NEW
+	if player:
+		player.get_node("HealthTimer").start(20.0)  # NEW: 20 "seconds" = 20 health
